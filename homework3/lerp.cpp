91d7b25920316488f431749708ec2fd653768ca0
Mariia Sharkovska <99342281+mariiasharkovskaya@users.noreply.github.com>
2024-03-29 14:01:36 +0200

    Update lerp.cpp

--- homework3/lerp.cpp
+++ homework3/lerp.cpp
@@ -4,27 +4,27 @@ int main()
 {
     double arr[2][5] = { {1, 2, 3, 4, 5}, {1, 4, 9, 16, 25} };
     double x = 2.9;
+    int start = -1;
+    int end = -1;
+    double y0, y1, x0, x1, result;
     for (int i = 0; i < 5; i++)
     {
-        double y0, y1, x0, x1, result;
-        int start, end;
-        // std::cout << (arr[0][i]) << " ";
         if(arr[0][i] > x)
         {
-            double t = 0.5;
-            // std::cout << std::endl << i << std::endl;
             start = i - 1;
             end = i;
-            x0 = arr[0][start];
-            x1 = arr[0][end];
-            y0 = arr[1][start];
-            y1 = arr[1][end];
-            std::cout << "y0 = " << y0 << " y1 = " << y1 << std::endl;
-            //result = y0 * (1 - t) + y1 * t;
-            result = y0 + (x - x0) * ((y1 - y0) / (x1 - x0));
-            std::cout << result;
             break;
         }
     }
-    
+    if (start == -1 || end == -1) {
+        std::cout << "x is outside the range of values in the array" << std::endl;
+		return 1;
+    }
+    x0 = arr[0][start];
+    x1 = arr[0][end];
+    y0 = arr[1][start];
+    y1 = arr[1][end];
+    std::cout << "y0 = " << y0 << " y1 = " << y1 << std::endl;
+    result = y0 + (x - x0) * ((y1 - y0) / (x1 - x0));
+    std::cout << "Interpolated value of y for x = " << x << ": " << result;
 }
